62dee907fcda84cf06eef83fc5af97d2e6281a03
Zach Deibert <zachdeibert@gmail.com>
2018-01-10 20:05:38 -0600

    Created project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.ZachDeibert.ProxyConfigurer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
